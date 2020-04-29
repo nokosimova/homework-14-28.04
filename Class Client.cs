3efd0f27b39878c1ada740f3782692f692777424
unknown <nkosimova06@gmail.com>
2020-04-29 22:36:52 +0500

    fix methods in Client class

--- Class Client.cs
+++ Class Client.cs
@@ -12,12 +12,36 @@ public class Client
     string MiddleName{get; set;}
     
     public void Insert(ref List<Client> ClientList)
-    {}
-    public void Update(ref Client client)
-    {}
-    public void Delete(ref Client client)
-    {}
-    public void Select(Client client)
-    {}
+    {
+        Client newclient = new Client();
+
+        newclient.id = this.id;
+        newclient.Balance = this.Balance;
+        newclient.FirstName = this.FirstName;
+        newclient.LastName = this.LastName;
+        newclient.MiddleName = this.MiddleName;
+
+        ClientList.Add(newclient);
+    }
+    public void Update(ref Client client, string FirstName = null, string LastName = null,string MiddleName = null, decimal Balance = -1)
+    {
+        if (FirstName != null) client.FirstName = FirstName;
+        if (LastName != null) client.LastName = LastName;
+        if (MiddleName != null) client.MiddleName = MiddleName;
+        if (Balance != -1 && Balance >= 0) client.Balance = Balance;
+    }
+    public void Delete(ref List<Client> ClientList)
+    {
+        ClientList.Remove(this);
+    }
+    public static Client Select(List<Client> ClientList, Client client)
+    {
+        Client ans = new Client();
+        foreach (Client c in ClientList)
+        {
+            if (c == client) ans = c;
+        }
+        return ans;
+    }
 }
 }
