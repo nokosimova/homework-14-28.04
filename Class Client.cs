b7f7e83b8db4fcff545296339b2c337c6cdaa9f4
unknown <nkosimova06@gmail.com>
2020-05-06 02:13:51 +0500

    final code

--- Class Client.cs
+++ Class Client.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using HW28._04;
 
-namespace Class
+
+namespace HW28._04
 {
-
-
 public class Client
 {
     public int Id{get; set;}
@@ -14,7 +12,7 @@ public class Client
     public string FirstName{get; set;}
     public string LastName{get; set;}
     public string MiddleName{get; set;}
-    public int Age{get; set;}
+    public int? Age{get; set;}
     
     public Client()
     {
@@ -24,21 +22,22 @@ public class Client
         MiddleName = null;
         CurrentBalance = -1;
         PreviousBalance  = -1;
-        Age = 0;
+        Age = null;
     }
-    public Client(int id = 0, string firstname = null, string lastname = null, string midname = null, decimal Currentbalance = -1, int age = 0)
+    public Client(int id = 0, string firstname = null, string lastname = null, string midname = null, decimal Currentbalance = -1, int? age = null)
     {
         Id = id;
         FirstName = firstname;
         LastName = lastname;
         MiddleName = midname;
         CurrentBalance = Currentbalance;
+        PreviousBalance  = -1;
         Age = age;
     }
-    public static void Insert(object obj, List<Client> MainList)
+    public static void Insert(object obj)
     {
         Client client = obj as Client;
-        MainList.Add(client);  
+        Program.MainClientList.Add(client);  
         Console.WriteLine("---------------------------------");
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Client was successfully inserted!");
@@ -46,24 +45,24 @@ public class Client
         Console.WriteLine("---------------------------------");
                                   
     }
-    public static void UpdateById(object obj, List<Client> MainList)
+    public static void UpdateById(object obj)
     {
         Client updatedClient = obj as Client;
-        for(int i = 0; i < MainList.Count; i++)
+        for(int i = 0; i < Program.MainClientList.Count; i++)
         {
-            if (MainList[i].Id == updatedClient.Id)
+            if (Program.MainClientList[i].Id == updatedClient.Id)
             {
-                if (updatedClient.FirstName != null) MainList[i].FirstName = updatedClient.FirstName;
-                if (updatedClient.LastName != null) MainList[i].LastName = updatedClient.LastName;
-                if (updatedClient.MiddleName != null) MainList[i].MiddleName = updatedClient.MiddleName;
-                if (updatedClient.CurrentBalance != -1 && updatedClient.CurrentBalance >= 0) MainList[i].CurrentBalance = updatedClient.CurrentBalance;
-                if (updatedClient.Age != 0 && updatedClient.Age > 0) MainList[i].Age = updatedClient.Age;
+                if (updatedClient.FirstName != null) Program.MainClientList[i].FirstName = updatedClient.FirstName;
+                if (updatedClient.LastName != null) Program.MainClientList[i].LastName = updatedClient.LastName;
+                if (updatedClient.MiddleName != null) Program.MainClientList[i].MiddleName = updatedClient.MiddleName;
+                if (updatedClient.Age != null || updatedClient.Age > 0) Program.MainClientList[i].Age = updatedClient.Age;
                 if (updatedClient.CurrentBalance != -1 && updatedClient.CurrentBalance >= 0)
                 { 
-                    if (MainList[i].PreviousBalance != MainList[i].CurrentBalance)
-                        MainList[i].PreviousBalance = MainList[i].CurrentBalance;
-                    MainList[i].CurrentBalance = updatedClient.CurrentBalance;
+                    if (Program.MainClientList[i].PreviousBalance == -1)
+                        Program.MainClientList[i].PreviousBalance = Program.MainClientList[i].CurrentBalance;
+                    Program.MainClientList[i].CurrentBalance = updatedClient.CurrentBalance;
                 }
+                Console.WriteLine("--------------------------------");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Client was successfully updated!");
                 Console.ForegroundColor = ConsoleColor.Gray;
@@ -71,36 +70,63 @@ public class Client
             }
         }
     }
-    public static void Delete(object obj, List<Client> MainList)
+    public static void Delete(object obj)
     {
-        Client deletingClient = obj as Client;
-        MainList.Remove(deletingClient);
+        Client deletingClient = new Client();
+        int ClientId = (int)obj;
+        for (int i = 0; i < Program.MainClientList.Count; i++)
+        {
+            if (Program.MainClientList[i].Id == ClientId) 
+            {
+                Program.MainClientList.RemoveAt(i);
+                Console.WriteLine("---------------------------------");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Client was successfully deleted!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("---------------------------------");
+            }
+        }
     }
-    public static bool SelectById(object obj, List<Client> MainList)
+    public static bool SelectById(object obj)
     {
         bool clientExistStatus = false;
         int selectedClientId = (int)obj;
-        for(int i = 0; i < MainList.Count; i++)
+        foreach(Client c in Program.MainClientList)
         {
-            Client c = MainList[i];
             if (c.Id == selectedClientId)
             {
                 clientExistStatus = true;
                 Console.Write("|id   |FIO                |Age     |CurrentBalance \n");
                 Console.WriteLine("----------------------------------------");
-                Console.WriteLine($"{c.Id,-3} {c.FirstName+c.LastName+c.MiddleName,-9} {c.Age,-5}{c.CurrentBalance,-15}");
+                Console.WriteLine($"{c.Id,-3} {c.FirstName+c.LastName+c.MiddleName} {c.Age,-5}{c.CurrentBalance,-15}");
+                break;
             }
-            break;
         }
+        if (!clientExistStatus) Console.WriteLine("Error: No client with such id!");
         return clientExistStatus;
     }
-    public static void SelectAll(List<Client> MainList)
+    public static void SelectAll()
     {
-        Console.WriteLine($"COUNT:   {MainList.Count}");
-        Console.Write("|id   |FIO                |Age     |CurrentBalance \n");
+        Console.Write("|id   |FIO                   |CurrentBalance \n");
         Console.WriteLine("----------------------------------------");
-        foreach(Client c in MainList)
-            Console.WriteLine($"{c.Id,-3} {c.FirstName+c.LastName+c.MiddleName,-9} {c.Age,-5}{c.CurrentBalance,-15}");           
+        foreach(Client c in Program.MainClientList)
+            Console.WriteLine($"{c.Id,-3} {c.FirstName+c.LastName+c.MiddleName}  {c.Age}    {c.CurrentBalance,-5}");           
+        Console.WriteLine("--------------------------------");
     }
 }
 }
+
+/* Данное домашнее задание оказалось для меня очень трудным, потом что
+в процессе работы я столкнулась с кучей-кучей проблем, возможно это видно из моего
+постоянного переделывания кода. Перечислю некоторые из них
+1) Одной из больших глупостей было то, что я не 
+поискав как следует ответ на вопрос: Как использовать статическую переменную из Program.cs в 
+реализации классов в Classes.cs, поэтому я очень усложнила себе работу.
+2) По окончанию написания кода я поняла что использовала слишком длинные
+названия переменных, что в конечном итоге сделало мой год ооочень тяжёлым для чтения
+но для исправления этого у меня не отсалось никаких эмоциональных сил
+3) Так как данное ДЗ я сдавала не в срок, то постаралась максимально продумать
+и протестировать ошибки в каждом пункте. И так и вышло: в написании каждой операции
+(Insert, Update, Delete, ...) я делала больше 5-6 ошибок, что в общем-то очень полезно,
+ведь теперь я выучила где и что находится в моём коде
+*/
--- Program.cs
+++ Program.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using Class;
 using System.Threading;
+/* Данное домашнее задание оказалось для меня очень трудным, потом что
+в процессе работы я столкнулась с кучей-кучей проблем, возможно это видно из моего
+постоянного переделывания кода. Перечислю некоторые из них
+1) Одной из больших глупостей было то, что я не 
+поискав как следует ответ на вопрос: Как использовать статическую переменную из Program.cs в 
+реализации классов в Classes.cs, поэтому я очень усложнила себе работу.
+2) По окончанию написания кода я поняла что использовала слишком длинные
+названия переменных, что в конечном итоге сделало мой год ооочень тяжёлым для чтения
+но для исправления этого у меня не отсалось никаких эмоциональных сил
+3) Так как данное ДЗ я сдавала не в срок, то постаралась максимально продумать
+и протестировать ошибки в каждом пункте. И так и вышло: в написании каждой операции
+(Insert, Update, Delete, ...) я делала больше 5-6 ошибок, что в общем-то очень полезно,
+ведь теперь я выучила где и что находится в моём коде
+*/
 
 namespace HW28._04
 {
     class Program
     {
-        static List<Client> MainClientList = new List<Client>();
+        public static List<Client> MainClientList = new List<Client>();
         static int LastId = 0;
         static void Main(string[] args)
         {
+            Client client = new Client();
             bool command = true;
             decimal balance;
-            int age;
-            string fname, lname, mname;
-            
-            
-            
-           
-            Thread SelectThread = new Thread(new ThreadStart(()=>{}));
-            
+            int? age = null;
+            string fname, lname, mname;   
             while(command)
             {
+           //     if (Console.BackgroundColor != ConsoleColor.Gray) 
+           //         Console.BackgroundColor = ConsoleColor.Gray;
                 Console.WriteLine("Choose action: ");
                 Console.WriteLine("------------------------------");
                 Console.WriteLine("Insert client--1");
@@ -34,21 +44,22 @@ namespace HW28._04
                 Console.WriteLine("--------------------------------");
                 int action = int.Parse(Console.ReadLine());
                 TimerCallback callback = new TimerCallback((i)=>BalanceChangingStatistic());
-                Timer timer = new Timer(callback, null, 0, 1000);
+                Timer timer = new Timer(callback, null, 0, 2000);
                 switch(action)
                 {
                     case 1:
                         Console.Write("FirstName: ");
                         fname = Console.ReadLine();
                         
-                        Console.Write("LastName:     ");
+                        Console.Write("LastName:");
                         lname = Console.ReadLine();
 
                         Console.Write("MiddleName:");
                         mname = Console.ReadLine();
 
                         Console.Write("Age:");
-                        age = int.Parse(Console.ReadLine());
+                        object s = Console.ReadLine();
+                        if (s != null) age = s as int?;
 
                         Console.Write("Balance:   ");
                         balance = decimal.Parse(Console.ReadLine());
@@ -61,29 +72,30 @@ namespace HW28._04
                                 break;
                             }
                         Client newclient = new Client(++LastId, fname, lname, mname, balance, age);                        
-                        Thread InsertThread = new Thread(new ThreadStart(()=>Client.Insert(newclient,MainClientList)));
+                        Thread InsertThread = new Thread(new ThreadStart(()=>Client.Insert(newclient)));
+                        InsertThread.Start();
+                        Thread.Sleep(400);
                         break;
                     case 2:
                         Console.Write("Choose id: ");
                         int id= int.Parse(Console.ReadLine());
-
-                        if (!Client.SelectById(id, MainClientList))
-                            Console.WriteLine("Error: No client with such id!");
-                        else
+                       
+                        if (Client.SelectById(id))
                         {
                             Console.WriteLine("Write new data, if necessary, or click enter near field, if not.");
                             Console.Write("FirstName: ");
                             fname = Console.ReadLine();
 
-                            Console.Write("LastName:     ");
+                            Console.Write("LastName:");
                             lname = Console.ReadLine();
 
                             Console.Write("MiddleName:");
                             mname = Console.ReadLine();
 
                             Console.Write("Age:");
-                            age = int.Parse(Console.ReadLine());
-
+                            s = Console.ReadLine();
+                            if (s != null) age = s as int?;
+                            
                             Console.Write("Balance:   ");
                             balance = decimal.Parse(Console.ReadLine());
                             if (balance <= 0){
@@ -95,36 +107,31 @@ namespace HW28._04
                                 break;
                             }
                             Client updatedClient = new Client(id, fname, lname, mname, balance, age);
-                            Thread UpdateThread = new Thread(new ThreadStart(()=>{Client.UpdateById(updatedClient,MainClientList);}));
+                            Thread UpdateThread = new Thread(new ThreadStart(()=>{Client.UpdateById(updatedClient);}));
+                            UpdateThread.Start();
+                            Thread.Sleep(400);
                         }                    
                         break;
                     case 3:
                         Console.Write("Choose Id of client to delete: ");
                         id= int.Parse(Console.ReadLine());
-
-                        if (!Client.SelectById(id, MainClientList))
-                            Console.WriteLine("Error: No client with such id!");
-                        else 
-                        { 
-                            Thread DeleteThread = new Thread(new ThreadStart(()=>Client.Delete(id, MainClientList)));
-                            Console.WriteLine($"Client with id {id} was deleted");
-                        }                   
+                        
+                        Thread DeleteThread = new Thread(new ThreadStart(()=>Client.Delete(id)));
+                        DeleteThread.Start();         
+                        Thread.Sleep(400);                                      
                         break;
                     case 4:
                         Console.Write("Choose id: ");
                         id= int.Parse(Console.ReadLine());
-
-                        if (!Client.SelectById(id, MainClientList))
-                            Console.WriteLine("Error: No client with such id!");
-                        else
-                        {
-                            Thread DeleteThread = new Thread(new ThreadStart(()=>Client.SelectById(id, MainClientList)));
-                            Console.WriteLine("--------------------------------");
-                        }
+                        Thread SelectThread = new Thread(new ThreadStart(()=>Client.SelectById(id)));
+                        SelectThread.Start();
+                        Console.WriteLine("--------------------------------");
+                        Thread.Sleep(400);
                         break;
                     case 5:
-                        Thread SelectAllThread = new Thread(new ThreadStart(()=>Client.SelectAll( MainClientList)));
-                        Console.WriteLine("--------------------------------");
+                        Thread SelectAllThread = new Thread(new ThreadStart(()=>Client.SelectAll()));
+                        SelectAllThread.Start();
+                        Thread.Sleep(400);
                         break;
                     case 6:
                         Console.WriteLine("Bye!");
@@ -132,11 +139,10 @@ namespace HW28._04
                         command = false;
                         break;
                     default:
-                        Console.WriteLine("Error: Wrong command. Try again!");
-                        Console.WriteLine("--------------------------------");
-                        break;
-                   
-
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong commad! Try again!");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("---------------------------------");break;
                 }
             }
         }
@@ -147,23 +153,23 @@ namespace HW28._04
             decimal difference = 0;
             foreach(Client c in MainClientList)
             {
-                if (balancechangestatus)
-                {
-                    Console.WriteLine("Balance changings list:");
-                    Console.WriteLine("|Id     |PrevBalance|Curbalance |Difference|");
-                    Console.WriteLine("-------------------------------");
-                }
                 if (c.PreviousBalance != -1)
                 {
-                    if (!balancechangestatus) balancechangestatus = true;
-
+                    if (!balancechangestatus)
+                    {
+                        Console.WriteLine("Balance changings list:");
+                        Console.WriteLine("|Id     |PrevBalance|Curbalance |Difference|");
+                        Console.WriteLine("-------------------------------");
+                        balancechangestatus = balancechangestatus = true;
+                    }
                     Console.ForegroundColor = (c.PreviousBalance > c.CurrentBalance)? ConsoleColor.Red :ConsoleColor.Green; 
                     difference = Math.Abs(c.PreviousBalance - c.CurrentBalance);
                     sign = (c.PreviousBalance > c.CurrentBalance) ? '-' : '+';
-                    Console.WriteLine($"{c.Id,-9} {c.PreviousBalance,-5} {c.CurrentBalance, -5}{difference,-5}");
+                    Console.WriteLine($"{c.Id,-9} | {c.PreviousBalance,-5} | {c.CurrentBalance, -5}  | {sign} {difference,-5}");
                     c.PreviousBalance = -1;
                 }
             }
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
 
     }
